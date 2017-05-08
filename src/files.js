4431a9cab94a4f0560606fbd107898f5febe8351
Mauro Junior <mauropereirajr@gmail.com>
2017-05-08 17:35:48 -0300

    feat: added feature to copy template based in module type

--- src/files.js
+++ src/files.js
@@ -1,16 +1,40 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
+import copydir from 'copy-dir';
+import * as path from 'path';
 
-export default {
-  getCurrentDirectoryBase() {
-    return path.basename(process.cwd());
-  },
 
-  directoryExists(filePath) {
-    try {
-      return fs.statSync(filePath).isDirectory();
-    } catch (err) {
+export default Object.freeze({
+  getCurrentDirectoryBase,
+
+  copyModule,
+
+  directoryExists
+});
+
+
+export function copyModule(moduleName, toFolder, done) {
+  const fromPath = path.resolve(__dirname, '../node_modules/', moduleName);
+  const toPath = path.resolve(process.cwd(), toFolder);
+
+  copydir(fromPath, toPath, (stat, filePath, fileName) => {
+    if (stat === 'file' && fileName === 'README.md') {
       return false;
     }
+
+    return true;
+  }, done);
+}
+
+
+export function getCurrentDirectoryBase() {
+  return path.basename(process.cwd());
+}
+
+
+export function directoryExists(filePath) {
+  try {
+    return fs.statSync(filePath).isDirectory();
+  } catch (err) {
+    return false;
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import clear from 'clear';
 import inquirer from 'inquirer';
 import Spinner from './console/spinner';
 
-import files from './files';
+import {
+  getCurrentDirectoryBase,
+  directoryExists,
+  copyModule
+} from './files';
 
 const print = data => console.log(data); // eslint-disable-line
 const banner = chalk.green(figlet.textSync('node-boilerplate', { font: 'Bubble' }));
 // Slant Bubble Rounded
+const spinner = Spinner();
+
 clear();
 print(`\n${banner}\n`);
 
@@ -20,11 +26,11 @@ function getAnswers(done) {
       message: 'Enter the `name` of your new module:\n',
       validate(value) {
         if (!value.length) {
-          return 'Enter the `name` of your new module!';
+          return chalk.red('Enter the `name` of your new module!');
         }
-        if (files.directoryExists(value)) {
+        if (directoryExists(value)) {
           return chalk.red(`Already exists a directory ${chalk.blue(`"${value}"`)
-            } in your current folder ${chalk.blue(`"${files.getCurrentDirectoryBase()}"`)}.`);
+            } in your current folder ${chalk.blue(`"${getCurrentDirectoryBase()}"`)}.`);
         }
 
         return true;
@@ -38,33 +44,31 @@ function getAnswers(done) {
       filter(val) {
         return val.toLowerCase();
       }
-    },
-    {
-      type: 'input',
-      name: 'type',
-      message: 'Choose a number?\n',
-      validate(input) {
-        const spinner = Spinner();
-
-        spinner.setText('checking the name...');
-        spinner.start();
-
-        // Declare function as asynchronous, and save the done callback
-        const done = this.async();
-
-        // Do async stuff
-        setTimeout(() => {
-          spinner.stop();
-          if (typeof input !== 'number') {
-            // Pass the return value in the done callback
-            done('You need to provide a number');
-            return;
-          }
-          // Pass the return value in the done callback
-          done(null, true);
-        }, 3000);
-      }
     }
+    // {
+    //   type: 'input',
+    //   name: 'type',
+    //   message: 'Choose a number?\n',
+    //   validate(input) {
+    //     spinner.setText('checking the name...');
+    //     spinner.start();
+
+    //     // Declare function as asynchronous, and save the done callback
+    //     const done = this.async();
+
+    //     // Do async stuff
+    //     setTimeout(() => {
+    //       spinner.stop();
+    //       if (typeof input !== 'number') {
+    //         // Pass the return value in the done callback
+    //         done('You need to provide a number');
+    //         return;
+    //       }
+    //       // Pass the return value in the done callback
+    //       done(null, true);
+    //     }, 3000);
+    //   }
+    // }
   ];
 
   inquirer.prompt(questions).then(done);
@@ -72,4 +76,18 @@ function getAnswers(done) {
 
 getAnswers(({ name, type }) => {
   print(`${chalk.blue(name)} - ${chalk.yellow(type)}`);
+  if (type === 'node') {
+    spinner.setText('copying files...');
+    spinner.start();
+    copyModule('node-module-boilerplate', name, (err) => {
+      spinner.stop();
+      if (err) {
+        print(err);
+        process.exit(1);
+      }
+      spinner.stop();
+      print(chalk.green(`Files created in ${chalk.blue(`./${name}`)} directory.`));
+      process.exit();
+    });
+  }
 });
